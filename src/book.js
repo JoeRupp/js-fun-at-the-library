43e06588c8682dee300fb47b3d1848ddff397895
Joe Rupp <joerpp8@gmail.com>
2022-02-02 21:52:10 -0700

    Add book part one

--- src/book.js
+++ src/book.js
@@ -1,11 +1,38 @@
-function createTitle() {
-  
+function createTitle(bookIdea) {
+  return `The ${bookIdea}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+}
+}
+
+// function saveReview(newReview, reviews) {
+//   for (var i = 0; i < reviews.length; i++) {
+//     if (newReview !== reviews[i]) {
+//       reviews.push(newReview);
+//     } else {
+//       return reviews;
+//     }
+//   }
+// }
+
+function saveReview(newReview, reviews) {
+    if (reviews.indexOf(newReview) === -1) {
+      reviews.push(newReview);
+      return reviews;
+    } else if (reviews.indexOf(newReview) > -1) {
+      return reviews;
+    }
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook

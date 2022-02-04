209002df03174633dd0f605220dd63e6362b71f7
Joe Rupp <joerpp8@gmail.com>
2022-02-03 19:02:58 -0700

    Add Book Tests Part 2

--- src/book.js
+++ src/book.js
@@ -7,33 +7,56 @@ function buildMainCharacter(name, age, pronouns) {
     name: name,
     age: age,
     pronouns: pronouns
+  }
 }
-}
-
-// function saveReview(newReview, reviews) {
-//   for (var i = 0; i < reviews.length; i++) {
-//     if (newReview !== reviews[i]) {
-//       reviews.push(newReview);
-//     } else {
-//       return reviews;
-//     }
-//   }
-// }
 
 function saveReview(newReview, reviews) {
-    if (reviews.indexOf(newReview) === -1) {
+  for (var i = 0; i < reviews.length + 1; i++) {
+    if (newReview !== reviews[i]) {
       reviews.push(newReview);
       return reviews;
-    } else if (reviews.indexOf(newReview) > -1) {
+    } else {
       return reviews;
     }
+  }
 }
+
+// Alternate method using indexOf.
+// indexOf - provides index position of element
+
+// function saveReview(newReview, reviews) {
+//     if (reviews.indexOf(newReview) === -1) {
+//       reviews.push(newReview);
+//       return reviews;
+//     } else if (reviews.indexOf(newReview) > -1) {
+//       return reviews;
+//     }
+// }
+
+function calculatePageCount(bookTitle) {
+  return (bookTitle.length * 20)
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre
+  }
+}
+
+function editBook(book) {
+  return book.pageCount = book.pageCount * .75
+}
+
+
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
